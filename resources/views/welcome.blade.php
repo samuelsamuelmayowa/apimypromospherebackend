54b83f18293552eed5723b041b59306f1dd2efb7
mayowasamuel <fpasamuelmayowa51@gmail.com>
2026-04-26 16:23:33 +0100

    dfsdgfds

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quia amet voluptatem dolore quas similique magni et omnis nostrum dignissimos odit, facere, optio quo? Enim eaque consequatur magni magnam in?
+    
+</body>
+</html>
